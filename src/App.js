ca2de86875d5ed08d05789cdbe866b0ddff2a210
VTU9730 <prathapsiddavatam@gmail.com>
2024-06-08 10:10:37 +0530

    contact-page-added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './Home'
+import Home from './Home';
+import Contact from './Contact'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
          <p>App component</p>
          <Home />
+         <Contact />
       </header>
     </div>
   );
