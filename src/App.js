29ad498f6f2fda215d07a59ffca1a14683f0f287
VTU9730 <prathapsiddavatam@gmail.com>
2024-06-08 10:04:21 +0530

    home-page-added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './Home'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
          <p>App component</p>
+         <Home />
       </header>
     </div>
   );
