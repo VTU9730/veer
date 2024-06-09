2298257fbc6e9de5bd5da8d13ab8a05a8e8c52cb
VTU9730 <prathapsiddavatam@gmail.com>
2024-06-09 09:58:20 +0530

    user-context-added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import appContext from './context';
+import appContext,{userContext} from './context';
 import Home from './Home';
 import Contact from './Contact'
 import About from './About'
@@ -8,9 +8,15 @@ import Email from './Email'
 import Address from './Address';
 import { useState } from 'react';
 function App() {
-  const [user,setUser]=useState('veera')
+  const [app,setApp]=useState('Demo')
+  const [user,setUser]=useState({
+    firstName:'Veeraprathap Reddy',
+    lastName:'Siddavatam',
+    education:'UG'
+  })
   return (
-    <appContext.Provider value={{user,setUser}}>
+    <appContext.Provider value={{app,setApp}}>
+    <userContext.Provider value={{user,setUser}}>
       <div className="App">
         <header className="App-header">
           <p>App component</p>
@@ -21,6 +27,7 @@ function App() {
           <Email />
         </header>
       </div>
+    </userContext.Provider>
     </appContext.Provider>
   );
 }
